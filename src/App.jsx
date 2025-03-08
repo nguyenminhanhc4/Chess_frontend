d59969249c4d8df95f5e3303c9cb607ca47c9abd
nguyen minh anh <nanh2832003@gmail.com>
2025-03-08 13:40:39 +0700

    thêm đường dẫn

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from "./layout/main";
 import Login from "./layout/Login";
+import HomePage from "./layout/homePage";
 import { UserAuthProvider } from "./context/UserAuthContext";
 function App() {
   return (
     <UserAuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<MainLayout />} />
+          <Route path="/main" element={<MainLayout />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
     </UserAuthProvider>
